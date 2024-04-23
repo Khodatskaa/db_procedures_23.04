d77da0970cc986717f6a4aff933fe58f905ae9b3
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-04-23 19:07:32 +0300

    Update SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+USE [db_procedure_practice_23.04];
+GO
+
+INSERT INTO ExampleTable (ID, Name, Date, Time, Value)
+VALUES 
+    (1, 'First Entry', '2024-04-23', '12:12:00', 15),
+    (2, 'Second Entry', '2024-04-24', '13:13:00', 13),
+    (3, 'Third Entry', '2024-04-25', '14:14:00', 14);
