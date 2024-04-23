21646d2bf9a5e6174d064f857312fa5570c4b37c
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-04-23 19:02:50 +0300

    Update SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,110 @@
+CREATE DATABASE [db_procedure_practice_23.04];
+GO
+USE [db_procedure_practice_23.04];
+GO
+
+CREATE TABLE ExampleTable (
+    ID INT PRIMARY KEY,
+    Name NVARCHAR(100),
+	Date DATE,
+	Time TIME,
+    Value INT
+);
+GO
+
+CREATE PROCEDURE HelloWorld
+AS
+BEGIN
+    PRINT 'Hello, world!';
+END;
+GO
+
+CREATE PROCEDURE GetCurrentTime
+AS
+BEGIN
+    SELECT GETDATE() AS CurrentTime;
+END;
+GO
+
+CREATE PROCEDURE GetCurrentDate
+AS
+BEGIN
+    SELECT CONVERT(date, GETDATE()) AS CurrentDate;
+END;
+GO
+
+CREATE PROCEDURE CalculateSum
+    @num1 INT,
+    @num2 INT,
+    @num3 INT
+AS
+BEGIN
+    SELECT @num1 + @num2 + @num3 AS Sum;
+END;
+GO
+
+CREATE PROCEDURE CalculateMean
+    @num1 INT,
+    @num2 INT,
+    @num3 INT
+AS
+BEGIN
+    SELECT (@num1 + @num2 + @num3) / 3.0 AS Mean;
+END;
+GO
+
+CREATE PROCEDURE GetMaxValue
+    @num1 INT,
+    @num2 INT,
+    @num3 INT
+AS
+BEGIN
+    SELECT MAX(val) AS MaxValue
+    FROM (VALUES (@num1), (@num2), (@num3)) AS Numbers(val);
+END;
+GO
+
+CREATE PROCEDURE GetMinValue
+    @num1 INT,
+    @num2 INT,
+    @num3 INT
+AS
+BEGIN
+    SELECT MIN(val) AS MinValue
+    FROM (VALUES (@num1), (@num2), (@num3)) AS Numbers(val);
+END;
+GO
+
+CREATE PROCEDURE GenerateLine
+    @length INT,
+    @symbol NVARCHAR(1)
+AS
+BEGIN
+    DECLARE @line NVARCHAR(MAX) = REPLICATE(@symbol, @length);
+    PRINT @line;
+END;
+GO
+
+CREATE PROCEDURE CalculateFactorial
+    @number INT
+AS
+BEGIN
+    DECLARE @factorial BIGINT = 1;
+    DECLARE @i INT = 1;
+    WHILE @i <= @number
+    BEGIN
+        SET @factorial = @factorial * @i;
+        SET @i = @i + 1;
+    END;
+    SELECT @factorial AS Factorial;
+END;
+GO
+
+CREATE PROCEDURE CalculatePower
+    @base INT,
+    @exponent INT
+AS
+BEGIN
+    SELECT POWER(@base, @exponent) AS Result;
+END;
+GO
